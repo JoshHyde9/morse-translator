ccc6f322e7c06f2c1ba0cce84f3c259af4c2d8ac
JoshHyde9 <joshhyde546@gmail.com>
2022-10-15 08:56:06 +1100

    feat: added encode and decode functions

---
+++ js/morse.mjs
@@ -0,0 +1,94 @@
+const textToMorse = new Map([
+  ["A", ".-"],
+  ["B", "-..."],
+  ["C", "-.-."],
+  ["D", "-.."],
+  ["E", "."],
+  ["F", "..-."],
+  ["G", "--."],
+  ["H", "...."],
+  ["I", ".."],
+  ["J", ".---"],
+  ["K", "-.-"],
+  ["L", ".-.."],
+  ["M", "--"],
+  ["N", "-."],
+  ["O", "---"],
+  ["P", ".--."],
+  ["Q", "--.-"],
+  ["R", ".-."],
+  ["S", "..."],
+  ["T", "-"],
+  ["U", "..-"],
+  ["V", "...-"],
+  ["W", ".--"],
+  ["X", "-..-"],
+  ["Y", "-.--"],
+  ["Z", "--.."],
+  ["1", ".----"],
+  ["2", "..---"],
+  ["3", "...--"],
+  ["4", "....-"],
+  ["5", "....."],
+  ["6", "-...."],
+  ["7", "--..."],
+  ["8", "---.."],
+  ["9", "----."],
+  ["0", "-----"],
+  [".", ".-.-.-"],
+  [",", "--..--"],
+  [",", "---..."],
+  ["?", "..--.."],
+  ["'", ".----."],
+  ["-", "-....-"],
+  ["/", "-..-."],
+  ["(", "-.--."],
+  [")", "-.--.-"],
+  ['"', ".-..-."],
+  ["@", ".--.-."],
+  ["=", "-...-"],
+  ["&", ".-..."],
+  ["+", ".-.-."],
+  ["!", "-.-.--"],
+  [" ", "/"],
+]);
+
+// Do the ol' switcheroo on the keys and values
+const morseToText = new Map([]);
+textToMorse.forEach((value, key) => {
+  morseToText.set(value, key);
+});
+
+/**
+ * Encode alphanumeric characters to morse code
+ * @param {string} text alphanumeric characters
+ * @returns {string} morse code
+ */
+export const encodeToMorse = (text) => {
+  return text
+    .split("")
+    .map((character) => {
+      return textToMorse.get(character.toUpperCase()) || "#";
+    })
+    .join(" ")
+    .replace(/ +/g, " ");
+};
+
+/**
+ * Decode morse code
+ * @param {string} text morse code
+ * @returns {string} alphanumeric characters
+ */
+export const decodeMorse = (text) => {
+  return text
+    .split("/")
+    .map((word) => {
+      return word
+        .split(" ")
+        .map((character) => {
+          return morseToText.get(character);
+        })
+        .join("");
+    })
+    .join(" ");
+};
