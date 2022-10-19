045b6183c8a4e06a1b56957df7f6e26f6e4cc557
JoshHyde9 <joshhyde546@gmail.com>
2022-10-20 08:14:02 +1100

    feat: added type checking test

--- js/__tests__/morse.test.mjs
+++ js/__tests__/morse.test.mjs
@@ -2,7 +2,13 @@ import { decodeMorse, encodeToMorse } from "../morse.mjs";
 
 describe("Encode English test", () => {
   it("should error passing in anything but a string", () => {
-    expect(checkType([])).toThrowError("Input is not a string");
+    expect(() => {
+      checkType([]);
+    }).toThrow("Input is not a string");
+
+    expect(() => {
+      checkType({ name: "Josh" });
+    }).toThrow("Input is not a string");
   });
 });
 
